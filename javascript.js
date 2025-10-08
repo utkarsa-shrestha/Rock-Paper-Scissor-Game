355993a86fc5ac3b1cf1bae26ee6337340036e50
Utkarsha Shrestha <shresthahom06@gmail.com>
2025-10-08 21:10:51 +0545

    Changes made in 8th October

--- javascript.js
+++ javascript.js
@@ -1,26 +1,33 @@
-const game = () =>{
-    const selected = document.querySelector('input[name="game"]:checked')
+const gamelogic =  () =>{
+    const selected = document.querySelector('input[name="game"]:checked');
     
-    let user_input = selected.value
+    let user_input = selected.value;
 
-    let index = getRandomInteger(0 ,2)
-    let game_value = ["Rock", "Paper", "Scissor"]
+    let index = getRandomInteger(0 ,2);
+    let game_value = ["Rock", "Paper", "Scissor"];
 
-    let computer_generated = game_value[index]
-    console.log(computer_generated)
-
+    let computer_generated = game_value[index];
+    console.log(computer_generated);
     if((user_input == "Rock" && computer_generated == "Scissor") || (user_input == "Paper" && computer_generated =="Rock") || (user_input == "Scissor" && computer_generated == "Paper")){
-        alert("You win!!")
+        document.querySelector("#win").style.display = "flex";
+        document.getElementById('win').classList.add('.highlight')
     }
     else if(user_input == computer_generated){
-        alert("Its a Draw")
+        alert("Its a Draw");
     }
     else{
-        alert("You lose")
+        alert("You lose");
     }
 
-    console.clear()
+    console.clear();
 }
+
+
+const closewindow =  () =>{
+    document.querySelector("#win").style.display = "none";
+    document.body.classList.remove()
+}
+
 const getRandomInteger = (min, max)=> { //to generate a random number
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
