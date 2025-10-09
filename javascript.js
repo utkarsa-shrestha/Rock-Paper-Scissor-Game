640fcfbaa6d3fff9617b0bf5f327075a9cf254d1
Utkarsha Shrestha <shresthahom06@gmail.com>
2025-10-09 20:32:57 +0545

    Final commit (maybe)

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-const gamelogic =  () =>{
+const gamelogic =  () =>{ //game logic function
+    const overlay = document.querySelector('.overview') //local const 
+
+
     const selected = document.querySelector('input[name="game"]:checked');
     
     let user_input = selected.value;
@@ -8,24 +11,36 @@ const gamelogic =  () =>{
 
     let computer_generated = game_value[index];
     console.log(computer_generated);
+
+    document.querySelector(".overview").style.display = "block"; //the dimming of the background
+
     if((user_input == "Rock" && computer_generated == "Scissor") || (user_input == "Paper" && computer_generated =="Rock") || (user_input == "Scissor" && computer_generated == "Paper")){
-        document.querySelector("#win").style.display = "flex";
-        document.getElementById('win').classList.add('.highlight')
+        document.querySelector("#win").style.display = "flex";  //display the panel
+        document.querySelector(".confetti-container").style.display = "block"; //confetti animation
     }
     else if(user_input == computer_generated){
-        alert("Its a Draw");
+        document.querySelector('#draw').style.display = "flex"; //display the panel
+        document.querySelector(".draw-container").style.display = "block" //draw animation
     }
     else{
-        alert("You lose");
+        document.querySelector('#lose').style.display = "block"; //display the panel
+        document.querySelector(".lose-container").style.display = "block"//lose animation
     }
 
-    console.clear();
+
 }
 
+const closewindow =  () =>{ //function to close panel
+    
+    const overlay = document.querySelector('.overview');//another local const
 
-const closewindow =  () =>{
-    document.querySelector("#win").style.display = "none";
-    document.body.classList.remove()
+    document.querySelector("#win").style.display = "none"; //remove the of win panel
+    document.querySelector('#lose').style.display = "none"; //remove the lose panel
+    document.querySelector("#draw").style.display = "none"; //remove the draw panel
+    document.querySelector('.overview').style.display = "none"; //removed the dimming
+    document.querySelector(".confetti-container").style.display = "none"//removed the cofetti animation
+    document.querySelector(".lose-container").style.display = "none"//remode the lose animation
+    document.querySelector(".draw-container").style.display = "none"//removed the draw animation
 }
 
 const getRandomInteger = (min, max)=> { //to generate a random number
